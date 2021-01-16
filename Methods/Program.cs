f9146f5a3c0c9fec639fbce75cff4e499e176d67
Engin Aydın <engin1637e@gmail.com>
2021-01-16 23:45:47 +0300

    16 Ocak Ders Sonu

---
+++ Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("----------Metotlar---------");
+            //instance - örnek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+        }
+    }
+}
+
+
+
+//Do not repeat yourself ( DRY ) 
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        //naming convention
+        //normal parentez görürsen anla ki metot çalışıyor
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi! : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, string fiyat)
+        {
+
+        }
+    }
+}
